758d45585cab7713ea353e5096d3929fbd53b607
Ioannis Papadopoulos <papadopoulos@mini100.wias-berlin.de>
2023-11-22 17:22:30 +0100

    move ADI routines, add plot function for zslice & get ADI working with hp-FEM on the interval

--- src/SparseDiskFEM.jl
+++ src/SparseDiskFEM.jl
@@ -1,9 +1,14 @@
 module SparseDiskFEM
 
-using RadialPiecewisePolynomials, LaTeXStrings, Plots, PyPlot, DelimitedFiles
+using ClassicalOrthogonalPolynomials, PiecewiseOrthogonalPolynomials, RadialPiecewisePolynomials, 
+    MultivariateOrthogonalPolynomials, LaTeXStrings, Plots, PyPlot, DelimitedFiles
 
-export plot, cylinder_plot_save, slice_plot, @L_str, writedlm,
-        write_adi_vals
+import RadialPiecewisePolynomials: _getMs_ms_js, Fill, blockedrange
+import MultivariateOrthogonalPolynomials: ModalTrav
+
+export plot, cylinder_plot_save, slice_plot, @L_str, writedlm, zplot,
+        write_adi_vals,
+        list_2_modaltrav, modaltrav_2_list, adi_2_modaltrav, adi_2_list
 
 include("adi.jl")
 
@@ -91,6 +96,46 @@ function slice_plot(iθ::Int, θs, rs, vals, points; ylabel=L"$u(x,y)$", cell_ed
         vline!(points, color=:black, linewidth=0.8, label="", linestyle=:dash)
     end
     Plots.display(p)
+end
+
+function zplot(iθ::Int, rs, z, vals; ttl=[], vminmax=[], xlabel=[], ylabel=[], title=[])
+    rrs = reverse.(rs)
+    K = lastindex(rrs)
+
+    rvals = [zeros(lastindex(rrs[1]), lastindex(z)) for k in 1:K]
+    for i in 1:lastindex(z)
+        for k in 1:K
+            rvals[k][:,i] = reverse(vals[i][k][:,iθ])
+        end
+    end
+
+
+    PyPlot.rc("font", family="serif", size=14)
+    rcParams = PyPlot.PyDict(PyPlot.matplotlib["rcParams"])
+    rcParams["text.usetex"] = true
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    if vminmax == []
+        vmin,vmax = minimum(minimum.(rvals)), maximum(maximum.(rvals))
+    else
+        vmin,vmax = vminmax[1], vminmax[2]
+    end
+    norm = PyPlot.matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+
+    pc = []
+    for k=1:K
+        pc = pcolormesh(rrs[k], z, rvals[k]', cmap="bwr", shading="gouraud", norm=norm)
+    end
+
+    ax.set_title(title)
+    ax.set_xlabel(xlabel, fontsize=20)
+    ax.set_ylabel(ylabel, fontsize=20)
+    if ttl != []
+        cbar = plt.colorbar(pc, pad=0.1)#, cax = cbar_ax)
+        cbar.set_label(ttl)
+    end
+    display(gcf())
+
 end
 
 end # module SparseDiskFEM
--- src/adi.jl
+++ src/adi.jl
@@ -25,3 +25,148 @@ function write_adi_vals(𝐳p, rs, θs, vals; path="src/plotting/")
         writedlm(path*"vals$i.log", valrθz[i]) 
     end
 end
+
+
+function modaltrav_2_list(Φ::FiniteContinuousZernike{T}, u::AbstractArray{Vector{T}}) where T
+    N, points = Φ.N, Φ.points
+    K = length(points) - 1
+    Ns, _, _ = _getMs_ms_js(N)
+
+    cs = []
+    u = ModalTrav.(u)
+    for i in 1:2N-1
+        v = zeros(T, Ns[i]-1, K)
+        for k in 1:K
+            v[:, k] = u[k].matrix[1:Ns[i]-1,i]
+        end
+        append!(cs, [pad(vec(v'), blockedrange(Fill(K, Ns[i]-1)))])
+    end
+    return cs
+end
+
+function adi_2_modaltrav(Φ::FiniteContinuousZernike{T}, wQ::Weighted{<:Any, <:Jacobi}, Us::AbstractArray, z::AbstractArray{T}) where T
+    N, points = Φ.N, Φ.points
+    K = length(points) - 1
+    Ns, _, _ = _getMs_ms_js(N)
+
+    Y =  [zeros(T, sum(1:N), length(z)) for k in 1:K]
+    for zi in 1:lastindex(z)
+        X = [zeros(T, Ns[1], 2N-1) for k in 1:K]
+        for k in 1:K
+            for n in 1:2N-1
+                us = Us[n][k:K:end, zi]
+                X[k][1:lastindex(us), n] = us
+            end
+            Y[k][:,zi] = ModalTrav(X[k])
+        end
+    end
+    return Y
+end
+
+
+function axes_Φ(N, points)
+    points[1] ≈ 0 && return blockedrange(Vcat(length(points)-1, Fill(length(points) - 1, N-2)))
+    blockedrange(Vcat(length(points), Fill(length(points) - 1, N-2)))
+end
+
+function _adi_2_list(Φ::FiniteContinuousZernike{T}, Us::AbstractArray, z::AbstractArray{T}; N=0) where T
+    points = Φ.points
+    K = length(points) - 1
+
+    N = N==0 ? Int((length(Us)+1)/2) : N
+    @assert N ≤ Φ.N
+
+    Ns = getNs(N)
+
+    Y = []
+    Fs = Φ.Fs
+    for zi in 1:lastindex(z)
+        cs = []
+        for n in 1:2N-1
+
+            # Nn = points[1] ≈ 0 ? Ns[n]-1 : Ns[n]
+            # v = zeros(T, Nn, K)
+            # for k in 1:K
+            #     v[:, k] = Us[n][k:K:end, zi]
+            # end
+            # append!(cs, [pad(vec(v'), blockedrange(Fill(K, Nn)))])
+
+            append!(cs, [pad(Us[n][:,zi], axes_Φ(Ns[n], points))])
+        end
+        append!(Y, [cs])
+    end
+    return Y
+
+
+
+    bubbles = zeros(T, N-2, K)
+    if first(points) ≈ 0
+        if K == 1
+            hats = [fs[1][1]]
+        else
+            hats = vcat([fs[i][1] for i in 2:K-1], fs[end][1:2])
+        end
+        bubbles[:,1] = fs[1][2:N-1]
+        for i in 2:K bubbles[:,i] = fs[i][3:N] end
+    else
+        hats = vcat([fs[i][1] for i in 1:K-1], fs[end][1:2])
+        for i in 1:K bubbles[:,i] = fs[i][3:N] end
+    end
+
+    pad(append!(hats, vec(bubbles')), axes(F,2))
+end
+
+adi_2_list(Φ::FiniteContinuousZernike{T}, wQ::Weighted{<:Any, <:Jacobi}, Us::AbstractArray, z::AbstractArray{T};N=0) where T = _adi_2_list(Φ, Us, z, N=N)
+adi_2_list(Φ::FiniteContinuousZernike{T}, Q::ContinuousPolynomial, Us::AbstractArray, z::AbstractArray{T};N=0) where T = _adi_2_list(Φ, Us, z, N=N)
+
+
+
+## Coefficient storage conversion
+function list_2_modaltrav(Ψ::FiniteZernikeBasis{T}, u::AbstractArray) where T
+    N, points = Ψ.N, Ψ.points
+    K = length(points) - 1
+    U = [zeros(T, N ÷ 2, 2N-1) for i in 1:K]
+    Ns, _, _ = _getMs_ms_js(N)
+    for k in 1:K
+        for i in 1:lastindex(Ns)
+            U[k][1:Ns[i], i] = u[i][k:K:end]
+        end
+    end
+    return ModalTrav.(U)
+end
+
+function modaltrav_2_list(Ψ::FiniteZernikeBasis{T}, u::AbstractArray{Vector{T}}) where T
+    N, points = Ψ.N, Ψ.points
+    K = length(points) - 1
+    Ns, _, _ = _getMs_ms_js(N)
+
+    cs = []
+    u = ModalTrav.(u)
+    for i in 1:2N-1
+        v = zeros(T, Ns[i], K)
+        for k in 1:K
+            v[:, k] = u[k].matrix[1:Ns[i],i]
+        end
+        append!(cs, [pad(vec(v'), blockedrange(Fill(K, Ns[i])))])
+    end
+    return cs
+end
+
+function adi_2_modaltrav(Ψ::FiniteZernikeBasis{T}, P::Legendre{T}, Us::AbstractArray, z::AbstractArray{T}) where T
+    N, points = Ψ.N, Ψ.points
+    K = length(points) - 1
+    Ns, _, _ = _getMs_ms_js(N)
+
+    Y =  [zeros(T, sum(1:N), length(z)) for k in 1:K]
+    for i in 1:lastindex(z)
+        X = [zeros(T, Ns[1], 2N-1) for k in 1:K]
+        for k in 1:K
+            for n in 1:2N-1
+                us = Us[n][k:K:end, i]
+                X[k][1:lastindex(us), n] = us
+            end
+            Y[k][:,i] = ModalTrav(X[k])
+        end
+    end
+    return Y
+end
